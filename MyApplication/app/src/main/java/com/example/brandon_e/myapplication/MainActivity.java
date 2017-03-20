465e98c264322b1ba072f9bd7d377dc7b984153b
ellisbrandon20 <ellisbrandon20@tamu.edu>
2017-03-20 00:27:19 -0500

    got something working with buttons

--- MyApplication/app/src/main/java/com/example/brandon_e/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/brandon_e/myapplication/MainActivity.java
@@ -5,19 +5,29 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "DemoInitialApp";
+    public static String media = "none";
 
+
+    public static String getMedia() {
+        return media;
+    }
+
+    public static void setMedia(String media) {
+        MainActivity.media = media;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button btn = (Button) findViewById(R.id.textButton);
-        btn.setOnClickListener(new View.OnClickListener() {
+        Button textBtn = (Button) findViewById(R.id.textButton);
+        textBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Log.i(TAG, "This is a magic log message!");
@@ -25,6 +35,27 @@ public class MainActivity extends AppCompatActivity {
                 // Toast gives a little pop up message would not be sufficiant for the applicaiton but could be used until we figure out a better pop up
                 Toast.makeText(getApplicationContext(), "This is Toast Magic", Toast.LENGTH_SHORT)
                         .show();
+
+                // set media type to "text"
+                setMedia("text");
+            }
+        });
+
+
+        ImageButton imageBtn_1 = (ImageButton) findViewById(R.id.imageButton_1);
+        imageBtn_1.setOnClickListener(new View.OnClickListener()  {
+            @Override
+            public void onClick(View v) {
+
+                if(getMedia().equals("text")){
+                    // Toast gives a little pop up message would not be sufficiant for the applicaiton but could be used until we figure out a better pop up
+                    Toast.makeText(getApplicationContext(), "Image_1", Toast.LENGTH_SHORT)
+                            .show();
+                }
+                else{
+                    Toast.makeText(getApplicationContext(), "Poopy", Toast.LENGTH_SHORT)
+                            .show();
+                }
             }
         });
     }
