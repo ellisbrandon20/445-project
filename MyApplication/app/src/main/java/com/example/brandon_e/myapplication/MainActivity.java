ed067e6ba6f0b95f00e079c428bf33b58aa98b67
ellisbrandon20 <ellisbrandon20@tamu.edu>
2017-03-31 12:30:12 -0500

    all text with buttons work

--- MyApplication/app/src/main/java/com/example/brandon_e/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/brandon_e/myapplication/MainActivity.java
@@ -33,7 +33,7 @@ public class MainActivity extends AppCompatActivity {
     // alert dialog for video and text
     public void createTextAlert(String animal) {
         AlertDialog.Builder a_builder = new AlertDialog.Builder(MainActivity.this);
-        a_builder.setMessage(animal + "!")
+        a_builder.setMessage("This is a " + animal + "!")
                 .setCancelable(true)
                 .setPositiveButton("Ok",new DialogInterface.OnClickListener() {
                     @Override
@@ -152,7 +152,200 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        // bird button
+        ImageButton birdBtn = (ImageButton) findViewById(R.id.bird_button);
+        birdBtn.setOnClickListener(new View.OnClickListener()  {
+            @Override
+            public void onClick(View v) {
 
+                if(getMedia().equals("text")){
+                    createTextAlert("Bird");
+                }
+                else{
+                    mediumErrorMsg();
+                }
+            }
+        });
+
+        // cat button
+        ImageButton catBtn = (ImageButton) findViewById(R.id.cat_button);
+        catBtn.setOnClickListener(new View.OnClickListener()  {
+            @Override
+            public void onClick(View v) {
+
+                if(getMedia().equals("text")){
+                    createTextAlert("Cat");
+                }
+                else{
+                    mediumErrorMsg();
+                }
+            }
+        });
+
+        // chicken button
+        ImageButton chickenBtn = (ImageButton) findViewById(R.id.chicken_button);
+        chickenBtn.setOnClickListener(new View.OnClickListener()  {
+            @Override
+            public void onClick(View v) {
+
+                if(getMedia().equals("text")){
+                    createTextAlert("Chicken");
+                }
+                else{
+                    mediumErrorMsg();
+                }
+            }
+        });
+
+        // frog button
+        ImageButton frogBtn = (ImageButton) findViewById(R.id.frog_button);
+        frogBtn.setOnClickListener(new View.OnClickListener()  {
+            @Override
+            public void onClick(View v) {
+
+                if(getMedia().equals("text")){
+                    createTextAlert("Frog");
+                }
+                else{
+                    mediumErrorMsg();
+                }
+            }
+        });
+
+        // cow button
+        ImageButton cowBtn = (ImageButton) findViewById(R.id.cow_button);
+        cowBtn.setOnClickListener(new View.OnClickListener()  {
+            @Override
+            public void onClick(View v) {
+
+                if(getMedia().equals("text")){
+                    createTextAlert("Cow");
+                }
+                else{
+                    mediumErrorMsg();
+                }
+            }
+        });
+
+        // elephant button
+        ImageButton elephantBtn = (ImageButton) findViewById(R.id.elephant_button);
+        elephantBtn.setOnClickListener(new View.OnClickListener()  {
+            @Override
+            public void onClick(View v) {
+
+                if(getMedia().equals("text")){
+                    createTextAlert("Elephant");
+                }
+                else{
+                    mediumErrorMsg();
+                }
+            }
+        });
+
+        // lion button
+        ImageButton lionBtn = (ImageButton) findViewById(R.id.lion_button);
+        lionBtn.setOnClickListener(new View.OnClickListener()  {
+            @Override
+            public void onClick(View v) {
+
+                if(getMedia().equals("text")){
+                    createTextAlert("Lion");
+                }
+                else{
+                    mediumErrorMsg();
+                }
+            }
+        });
+
+        // pig button
+        ImageButton pigBtn = (ImageButton) findViewById(R.id.pig_button);
+        pigBtn.setOnClickListener(new View.OnClickListener()  {
+            @Override
+            public void onClick(View v) {
+
+                if(getMedia().equals("text")){
+                    createTextAlert("Pig");
+                }
+                else{
+                    mediumErrorMsg();
+                }
+            }
+        });
+
+        // monkey button
+        ImageButton monkeyBtn = (ImageButton) findViewById(R.id.monkey_button);
+        monkeyBtn.setOnClickListener(new View.OnClickListener()  {
+            @Override
+            public void onClick(View v) {
+
+                if(getMedia().equals("text")){
+                    createTextAlert("Monkey");
+                }
+                else{
+                    mediumErrorMsg();
+                }
+            }
+        });
+
+        // snake button
+        ImageButton snakeBtn = (ImageButton) findViewById(R.id.snake_button);
+        snakeBtn.setOnClickListener(new View.OnClickListener()  {
+            @Override
+            public void onClick(View v) {
+
+                if(getMedia().equals("text")){
+                    createTextAlert("Snake");
+                }
+                else{
+                    mediumErrorMsg();
+                }
+            }
+        });
+
+        // duck button
+        ImageButton duckBtn = (ImageButton) findViewById(R.id.duck_button);
+        duckBtn.setOnClickListener(new View.OnClickListener()  {
+            @Override
+            public void onClick(View v) {
+
+                if(getMedia().equals("text")){
+                    createTextAlert("Duck");
+                }
+                else{
+                    mediumErrorMsg();
+                }
+            }
+        });
+
+        // penguin button
+        ImageButton penguinBtn = (ImageButton) findViewById(R.id.penguin_button);
+        penguinBtn.setOnClickListener(new View.OnClickListener()  {
+            @Override
+            public void onClick(View v) {
+
+                if(getMedia().equals("text")){
+                    createTextAlert("Penguin");
+                }
+                else{
+                    mediumErrorMsg();
+                }
+            }
+        });
+
+        // fishBtn button
+        ImageButton fishBtn = (ImageButton) findViewById(R.id.fish_button);
+        fishBtn.setOnClickListener(new View.OnClickListener()  {
+            @Override
+            public void onClick(View v) {
+
+                if(getMedia().equals("text")){
+                    createTextAlert("Fish");
+                }
+                else{
+                    mediumErrorMsg();
+                }
+            }
+        });
     }
 
 
